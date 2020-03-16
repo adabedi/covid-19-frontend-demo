21dd396b6b39255d4c634375517c1c7fe8e49765
adabedi <adrianna.bedychaj@gmail.com>
2020-03-16 17:19:08 +0000

    Updates

---
+++ precache-manifest.4bdcab5be98599939c8acd950751cbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1944845907f0bd8106be64ee163fc4",
+    "url": "/covid-19-frontend-demo/index.html"
+  },
+  {
+    "revision": "442f60e0c1e680cb8656",
+    "url": "/covid-19-frontend-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7ea9ab62dd450b87fe62",
+    "url": "/covid-19-frontend-demo/static/js/2.7de396fb.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/covid-19-frontend-demo/static/js/2.7de396fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442f60e0c1e680cb8656",
+    "url": "/covid-19-frontend-demo/static/js/main.877f9f8a.chunk.js"
+  },
+  {
+    "revision": "f8dd1e8dad199c5d14d4",
+    "url": "/covid-19-frontend-demo/static/js/runtime-main.b1e1b60c.js"
+  }
+]);
