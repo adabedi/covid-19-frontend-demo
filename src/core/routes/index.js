37898a772491c73728a208714528173fdfa63c3e
adabedi <adrianna.bedychaj@gmail.com>
2020-03-16 17:18:17 +0000

    gh-pages setup

--- src/core/routes/index.js
+++ src/core/routes/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Switch, BrowserRouter, HashRouter, Route} from 'react-router-dom';
+import { Switch, BrowserRouter, Route} from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import Authentication from '../../common/Pages/Authentication';
 import Dashboard from '../../common/Pages/Dashboard';
 import Intro from '../../common/Pages/Intro';
 
 export default () => (
-  <HashRouter basename='/'>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route path="/welcome" component={Intro} />
       <Route path="/sign-in" component={Authentication} />
       <PrivateRoute path="/" component={Dashboard} />
     </Switch>
-  </HashRouter>
+  </BrowserRouter>
 );
